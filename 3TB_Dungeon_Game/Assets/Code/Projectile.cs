471c98c4e9399c7a92538214d1535f4017fe6c9a
Dylan0003 <dylansward06@gmail.com>
2021-08-04 15:02:34 -0400

    Added Player Shooting
    Player now creates a projectile. However, there is a bug where the projectile destroys itself when hitting the player.

--- 3TB_Dungeon_Game/Assets/Code/Projectile.cs
+++ 3TB_Dungeon_Game/Assets/Code/Projectile.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+<<<<<<< Updated upstream
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +15,53 @@ public class Projectile : MonoBehaviour
     void Update()
     {
         
+=======
+    public float damage;
+    public string primaryTarget;
+    public bool isBouncing = false;
+    public int bouncesLeft = 0;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Collider2D collider = collision.collider;
+        if (collider.tag == primaryTarget)
+        {
+            if (primaryTarget == "Player")
+            {
+                collider.gameObject.GetComponent<PlayerController>().takeDamage(this.damage);
+            }
+            else
+            {
+                collider.gameObject.GetComponent<EnemyController>().takeDamage(this.damage);
+            }
+            this.destroy();
+        }
+        else if (collider.tag == "DestructableWall")
+        {
+            collider.gameObject.GetComponent<DestructableWall>().takeDamage(this.damage);
+            this.destroy();
+        }
+        else if (collider.tag == "Wall")
+        {
+            if (isBouncing)
+            {
+                bouncesLeft--;
+            }
+            else
+            {
+                this.destroy();
+            }
+        }
+        if (bouncesLeft <= 0)
+        {
+            this.destroy();
+        }
+    }
+
+    void destroy()
+    {
+        //Add bullet impact effects if needed.
+        Destroy(gameObject); //Destroy Bullet afterwards
+>>>>>>> Stashed changes
     }
 }
