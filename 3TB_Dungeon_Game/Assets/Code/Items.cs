6f4a30ef5c9441c75720f59f65bbe69ed2706e49
Dylan0003 <dylansward06@gmail.com>
2021-08-10 23:48:18 -0400

    Revert "Added in all weapons. Everything else is UI."
    This reverts commit c0ceff67392a5304e137642347f4c0aef44200b3.

--- 3TB_Dungeon_Game/Assets/Code/Items.cs
+++ 3TB_Dungeon_Game/Assets/Code/Items.cs
@@ -5,24 +5,13 @@ using UnityEngine;
 public class Items
 {
     //Create all the weapons
-    //public static item defaultWeapon = new item(2, 0, 2, false, false); <-- Example
-    public static item headphoneLauncher = new item(10, 0, 8, false, false); //Regular weapon
-    public static item automaticEarplugGun = new item(6, 0, 16, false, false); //Fast attack
-    public static item solarRayGun = new item(4, 0, 20, false, true); //Will be laser in updates, but now its a super fast
-    public static item alienGun = new item(8, 0, 10, true, false); //Bouncing
-    public static item fireworkLauncher = new item(100, 0, 3, false, false); //Slow One-hit obliterator
+    public static item defaultWeapon = new item(2, 2, 2); //temporary numbers
 
     //Create all the shields
-    //public static item defaultShield = new item(0.2f, 500, 500, 3); <-- Example
-    public static item earplugShield = new item(0.8f, 400, 400, 1);
-    public static item headphoneShield = new item(0.6f, 400, 400, 2);
-    public static item headphoneNCShield = new item(0.4f, 400, 400, 3); //NC = Noise Cancelling
-    public static item headphoneAndEarplugs = new item(0.2f, 400, 400, 3);
-    public static item noShield = new item(1.0f, 100, 100, 1); //Default
+    public static item defaultShield = new item(0.2f, 500, 500, 3); //temporary numbers
 
-    //Puts them in lists
-    public static item[] items = new item[10] {headphoneLauncher, automaticEarplugGun, solarRayGun, alienGun, fireworkLauncher,
-    earplugShield, headphoneShield, headphoneNCShield, headphoneAndEarplugs, noShield};
+    //Put them in lists
+    public static item[] items = new item[2] {defaultWeapon, defaultShield}; //Fill in later
     public static int itemsListMax = items.Length - 1;
 }
 
@@ -33,18 +22,14 @@ public class item
     //for weapons
 
     public int damage; //Amount of damage dealt
-    public int zero; //Placeholder
+    public int reloadSpeed; //Time between use
     public int bulletSpeed; //Bullet travel Speed
-    public bool bulletsBounce;
-    bool isLaser;
-    public item(int attack, int placeholder, int speed, bool hasBouncingBullets, bool ShootsALaser)
+    public item(int attack, int attackSpeed, int speed)
     {
         this.type = "Weapon";
         this.damage = attack;
-        this.zero = placeholder;
+        this.reloadSpeed = attackSpeed;
         this.bulletSpeed = speed;
-        this.bulletsBounce = hasBouncingBullets;
-        this.isLaser = ShootsALaser;
     }
 
     //for shields
